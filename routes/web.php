397b6602475581bf2315047bd65e8b94a65959a4
capstonestudent <arosales@student.passerellesnumeriques.org>
2020-03-16 22:11:34 +0800

    [UPDATE]

--- routes/web.php
+++ routes/web.php
@@ -36,20 +36,3 @@ Route::group(['middleware' => 'auth'], function () {
     Route::get('/Report', 'ReportController@index')->name('report');
     
 });
-
-// Route::get('test','ProjectController@getTask');
-// use App\Models\Project;
-// Route::get('test/{projectName}',function($projectName){
-
-//     $project = Project::create([
-//         'projectName' => $projectName,
-//         'created_by' => \Auth::user()->firstname.' '.\Auth::user()->lastname
-//     ]);
-
-//     dd($project);
-// });
-
-// use App\Models\User;
-// Route::get('test',function(){
-//     dd(User::all()->with('role')->get());
-// });
